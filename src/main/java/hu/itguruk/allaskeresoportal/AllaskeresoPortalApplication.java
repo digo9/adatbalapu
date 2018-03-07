a6fbedd90337986081bf5728d1f4225ceb3cc75f
Zsiros Dominik <zsiros.dominik@ext.t-systems.hu>
2018-03-07 09:57:27 +0100

    initial commit

---
+++ src/main/java/hu/itguruk/allaskeresoportal/AllaskeresoPortalApplication.java
@@ -0,0 +1,12 @@
+package hu.itguruk.allaskeresoportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllaskeresoPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllaskeresoPortalApplication.class, args);
+	}
+}
