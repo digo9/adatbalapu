fab70490a6995d11cf8c489e171aa6fb273413f9
Dominik Zsiros <zsdominik@outlook.com>
2018-05-06 10:20:33 +0200

    routing change

--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -12,12 +12,12 @@ import { NewOfferComponent } from './new-offer/new-offer.component';
 const appRoutes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '404', component: Page404Component },
-  { path: 'home', component: MainPageComponent },
-  { path: 'registration', component: RegistrationComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'joboffer', component: JobComponent },
-  { path: 'findjobs', component: FindJobsComponent },
-  { path: 'newoffer', component: NewOfferComponent },
+  { path: 'fooldal', component: MainPageComponent },
+  { path: 'regisztracio', component: RegistrationComponent },
+  { path: 'bejelentkezes', component: LoginComponent },
+  { path: 'allasajanlat', component: JobComponent },
+  { path: 'allaskereses', component: FindJobsComponent },
+  { path: 'ujajanlat', component: NewOfferComponent },
   { path: '**', redirectTo: '404', pathMatch: 'full' } // ennek utolsonak kell lenni mert mindent redirectel
 ];
 
@@ -25,4 +25,4 @@ const appRoutes: Routes = [
   imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
-export class AppRouting { }
+export class AppRouting {}
